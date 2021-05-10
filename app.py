72940103c5e1cb8560db656c7c7708e0978238dd
michaelrizzo2 <m_rizzo@live.com>
2021-05-10 19:23:49 -0400

    about to set up the routes.

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 #import statements
 from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
+
+#Setting up the app and the needed options
+app=Flask(__name__)
+app.config['DEBUG']=True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+
+#Setting up the database
+my_database=SQLAlchemy(app)
+
+#Setting up sql fields
+class City(my_database.model):
+    id=my_database.column(my_database.Integer,primary_key=True)
+    name=my_database.column(my_database.String(50),nullable=False)
