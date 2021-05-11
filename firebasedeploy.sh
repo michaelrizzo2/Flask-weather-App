ac6db2d3328ee1fa74caea14e656f5e9c08969a5
Michael Rizzo <m_rizzo@live.com>
2021-05-11 15:26:11 -0400

    most recent version of files

--- firebasedeploy.sh
+++ firebasedeploy.sh
@@ -1,7 +1,7 @@
 
 #This will build the images 
-gcloud builds submit --tag gcr.io/flask-weather-app-937e6/flask-fire
+gcloud builds submit --tag gcr.io/flask-weather-app-937e6/myflask
 #This will deploy the image
-gcloud beta run deploy --image gcr.io/flask-weather-app-937e6/flask-fire
+gcloud beta run deploy --image gcr.io/flask-weather-app-937e6/myflask
 #This will deploy the image to firebase
 firebase deploy
