001cbfa2c4b192d3c69a7fee7c0b9ba98296ee66
michaelrizzo2 <m_rizzo@live.com>
2021-05-12 11:16:07 -0400

    fixing unbould local error bug

--- public/server/app.py
+++ public/server/app.py
@@ -2,11 +2,11 @@
 import requests
 import os
 from flask import Flask,render_template,request
-from firebase import firebase,firestore 
-from firebase_admin import credentials
+from firebase import firebase
+from firebase_admin import credentials,firestore 
 import firebase_admin
 #setting up firebase database link
-cred = credentials.Certificate("service key.json")
+cred = credentials.Certificate("../../service key.json")
 firebase_admin.initialize_app(cred)
 firestore_database=firestore.client()
 #Setting up the app and the needed options
@@ -15,10 +15,9 @@ app=Flask(__name__)
 
 
 #Setting up the routes,
-@app.route('/',methods=["GET","POST"])
-def index():
-    if request.method=="POST":
-        new_city=request.form.get('city')
+@app.route('/')
+def home():
+    new_city=request.form.get('city')
 
     #This will be for getting all of the weather data
     url=f'http://api.openweathermap.org/data/2.5/weather?q={new_city}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
