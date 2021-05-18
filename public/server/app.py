b04ebe370275254d2a5d82aa409d4a80096018e2
michaelrizzo2 <m_rizzo@live.com>
2021-05-18 18:38:47 -0400

    added method for main page.

--- public/server/app.py
+++ public/server/app.py
@@ -15,24 +15,25 @@ app=Flask(__name__)
 
 
 #Setting up the routes,
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def home():
-    new_city=request.form.get('city')
+    if request.method=="POST":
+        new_city=request.form.get('city')
 
-    #This will be for getting all of the weather data
-    url=f'http://api.openweathermap.org/data/2.5/weather?q={new_city}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+        #This will be for getting all of the weather data
+        url=f'http://api.openweathermap.org/data/2.5/weather?q={new_city}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
 
-    #This will be for putting the data insode of an json object
-    #Setting up the json 
-    city_json_object=requests.get(url.format(new_city.name)).json()
-    #putting json data into weather dictionary
-    weather={
-        'city':new_city,
-        'description':city_json_object['weather'][0]['description'],
-        'temperature':city_json_object['main']['temp'],
-        }
-        #adding new dictionary to the list.
-    firestore_database.collection(u'weather').add(weather)
+        #This will be for putting the data insode of an json object
+        #Setting up the json 
+        city_json_object=requests.get(url.format(new_city.name)).json()
+        #putting json data into weather dictionary
+        weather={
+            'city':new_city,
+            'description':city_json_object['weather'][0]['description'],
+            'temperature':city_json_object['main']['temp'],
+            }
+            #adding new dictionary to the list.
+        firestore_database.collection(u'weather').add(weather)
 
     return render_template('weather.html',weather_data=weather)
 
